ad7b4b521cfa8a3db1e4612e7bc359270974c9a8
nelsonGX <hi@nelsongx.com>
2026-02-07 00:55:49 +0800

    add contents and fixes to servermanage

--- components/sections/projects/ServerManage.tsx
+++ components/sections/projects/ServerManage.tsx
@@ -1,16 +1,22 @@
 import Image from "next/image"
+import Link from "next/link";
+import { ExternalLink } from "lucide-react";
 import { useTranslations } from 'next-intl'
 
 export default function ServerManager() {
   const t = useTranslations('projects.serverManage');
   return (
     <>
-    <h1 className="text-4xl py-10 text-center font-bold">{t('hosting.title')}</h1>
+    <h1 className="text-4xl py-10 md:px-16 font-bold text-orange-50">{t('hosting.title')}</h1>
     {[
       { 
         date: t('hosting.freeserver.date'), 
         title: 'FreeServer', 
         imageSrc: "/assets/images/freeserver.png", 
+        links: [
+          { name: t('hosting.freeserver.internet_archive_website'), url: 'https://web.archive.org/web/20220703070209/https://freeserver.fun/' },
+          { name: t('hosting.freeserver.discord'), url: 'https://discord.gg/nNyn7EK9PC' }
+        ],
         description: 
         <>
           <div>
@@ -28,6 +34,10 @@ export default function ServerManager() {
         date: t('hosting.cheapserver.date'), 
         title: 'CheapServer', 
         imageSrc: "/assets/images/CheapServer_white.png", 
+        links: [
+          { name: t('hosting.cheapserver.website'), url: 'https://cheapserver.tw/' },
+          { name: t('hosting.cheapserver.discord'), url: 'https://discord.gg/cheapserver' }
+        ],
         description: 
         <>
           <div>
@@ -62,6 +72,10 @@ export default function ServerManager() {
         date: t('hosting.freeserverv3.date'), 
         title: 'FreeServer v3', 
         imageSrc: "/assets/images/freeserverv3.png", 
+        links: [
+          { name: t('hosting.freeserverv3.website'), url: 'https://freeserver.tw/' },
+          { name: t('hosting.freeserverv3.discord'), url: 'https://discord.gg/k5GgFFxN2Q' }
+        ],
         description: 
         <>
           <div>
@@ -75,20 +89,27 @@ export default function ServerManager() {
       },
     ].map((data, idx) => (
       <div className="mb-8 md:flex md:space-x-6 space-y-4 md:space-y-0 items-center justify-center" key={idx}>
-        <div className="relative flex bg-zinc-800 shadow-xl w-24 h-12 rounded-lg">
-          <h1 className="mx-auto my-auto font-semibold text-lg text-white">{data.date}</h1>
+        <div className="relative flex bg-zinc-800 shadow-xl w-fit h-fit rounded-lg">
+          <h1 className="p-4 text-lg text-white">{data.date}</h1>
         </div>
-        <div className="md:flex bg-zinc-800 rounded-lg shadow-xl md:max-w-[66%] px-6 py-4">
+        <div className="md:flex bg-zinc-800 rounded-lg shadow-xl w-full md:max-w-[66%] px-6 py-4">
           <Image src={data.imageSrc} alt={'Image'} width={100} height={10} className="object-cover h-20 w-auto mx-auto md:mx-0 my-4 md:my-auto" />
-          <div className="relative px-4">
-              <h3 className="mb-3 font-bold text-white text-xl">{data.title}</h3>
-              <div className="text-sm leading-snug tracking-wide text-zinc-300 text-opacity-100">{data.description}</div>
+          <div className="relative px-4 space-y-4">
+            <h3 className="font-bold text-white text-xl">{data.title}</h3>
+            <div className="text-sm leading-snug tracking-wide text-zinc-300 text-opacity-100">{data.description}</div>
+            {data.links &&
+              <div className="bottom-4 right-6 flex space-x-4">
+                {data.links.map((link, linkIdx) => (
+                  <Link key={linkIdx} href={link.url} target="_blank" className="items-center flex text-sm text-orange-300 hover:text-orange-400 transition-all duration-150">
+                    {link.name} <ExternalLink className="inline-block w-4 h-4 ml-1" />
+                  </Link>
+                ))}
+              </div>
+            }
           </div>
         </div>
       </div>
     ))}
-
-    <h1 className="text-4xl py-10 text-center font-bold">{t('infrastructure.title')}</h1> 
     </>
   )
 }
