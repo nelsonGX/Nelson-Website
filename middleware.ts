5c7e385c2bf9c78d022361c7ae998456c70666dd
nelsonGX <hi@nelsongx.com>
2026-02-08 23:48:20 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import createMiddleware from 'next-intl/middleware';
 import {routing} from './i18n/routing';
 
-export const runtime = 'edge';
+export const runtime = 'experimental-edge';
  
 export default createMiddleware(routing);
  
